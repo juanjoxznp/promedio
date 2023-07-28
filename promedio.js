b3bf77e872eae37ce4f44058af33a4ce2d18c509
Juan Jose Calderon <juanjoxznp@gmail.com>
2023-07-28 10:56:36 -0500

    Promedio final

--- promedio.js
+++ promedio.js
@@ -3,3 +3,7 @@ alert("Bienvenido, calcularemos el promedio de 2 números")
 
 let numb1=Number(prompt("Ingrese el primer número"));
 let numb2=Number(prompt("Ingrese el segundo número"));
+let suma=numb1+numb2
+let prom=suma/2
+
+alert (`El promedio de los 2 numeros es ${prom}`)
